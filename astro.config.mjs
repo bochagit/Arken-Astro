4dbb59b8bfc5467d5b5bf9baa41fcbe063f6fe38
bochagit <gocardozo@alumno.unlam.edu.ar>
2026-04-06 10:37:32 -0300

    ESLint fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,14 @@
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+	server: {
+		host: true,
+		port: 4321
+	},
+	vite: {
+		server: {
+			allowedHosts: ['.tunnelmole.net']
+		}
+	}
+})
